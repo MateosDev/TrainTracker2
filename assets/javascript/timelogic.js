712d7b2b4826def5bebff6eb5a9eb0efd12b8ffc
MateosDev <35548023+MateosDev@users.noreply.github.com>
2019-12-15 07:34:17 -0600

    was missing unique id to populate data into new table under tbody tag, was missing the appropriate firebase database cdn link, small changes to styling, centered text in new data via CSS

--- assets/javascript/timelogic.js
+++ assets/javascript/timelogic.js
@@ -9,9 +9,70 @@ var config = {
   };
 
 
-// Initialize Database
-firebase.initializeApp(config);
-
-var trainInfo = firebase.database();
-// Testing connection
-console.log(trainInfo);
+  firebase.initializeApp(config);
+  var database = firebase.database();
+  
+  var train;
+  var destination;
+  var firstTime;
+  var frequency;
+  
+  $("#submit").on("click", function () {
+      event.preventDefault();
+  
+      var train = $("#train-name").val().trim();
+      var destination = $("#destination").val().trim();
+      var firstTime = $("#first-time").val().trim();
+      var frequency = $("#frequency").val().trim();
+  
+      database.ref().push({
+          train: train,
+          destination: destination,
+          firstTime: firstTime,
+          frequency: frequency,
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
+  
+  });
+  
+  function makeTable() {
+      database.ref().on("child_added", function (snapshot) {
+          var sv = snapshot.val();
+  
+          //first time adjusted to be in the past
+          var firstTimeConverted = moment(sv.firstTime, "hh:mm").subtract(1, "day");
+  
+          //difference between times
+          var timeDiff = moment().diff(firstTimeConverted, "minutes");
+  
+          //time until next train
+          var tMinutesTillTrain = sv.frequency - timeDiff % sv.frequency;
+  
+          //time of arrival of the next train
+          var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+          var nextTrainTime = moment(nextTrain).format("hh:mm");
+  
+          var newRow = $("<tr>");
+  
+          newRow.append($("<td>").text(sv.train));
+          newRow.append($("<td>").text(sv.destination));
+          newRow.append($("<td>").text(sv.frequency));
+          newRow.append($("<td>").text(nextTrainTime));
+          newRow.append($("<td>").text(tMinutesTillTrain));
+  
+          $("#table-body").append(newRow);
+  
+      }, function (errorObject) {
+          console.log("Errors handled: " + errorObject.code);
+      });
+  }
+  
+  makeTable();
+  
+  //update the table every minute
+  setInterval(updateTime, 60000);
+  
+  function updateTime() {
+      $("#table-body").html("");
+      makeTable();
+  }
