adb91a2a07d444867031ebbcbeab21954d7e1d34
MateosDev <35548023+MateosDev@users.noreply.github.com>
2019-12-15 06:39:36 -0600

    declared database var, connected firebase and copied config from the firebase website

--- assets/javascript/timelogic.js
+++ assets/javascript/timelogic.js
@@ -1,3 +1,12 @@
+var config = {
+    apiKey: "AIzaSyBsEbL2f2H5Pt44AZthZLJ4tHpAQy4qxko",
+    authDomain: "traintimersmuhw.firebaseapp.com",
+    databaseURL: "https://traintimersmuhw.firebaseio.com",
+    projectId: "traintimersmuhw",
+    storageBucket: "traintimersmuhw.appspot.com",
+    messagingSenderId: "608709981538",
+    appId: "1:608709981538:web:5c21557069710bd49ee83b"
+  };
 
 
 // Initialize Database
